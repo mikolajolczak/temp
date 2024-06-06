23db8f0d432609b56a8ce7dd7ae8271b08bb64be
mikolajolczak <80596840+mikolajolczak@users.noreply.github.com>
2024-06-06 10:46:15 +0200

    Update SPRINT.py

--- SPRINT.py
+++ SPRINT.py
@@ -15,7 +15,7 @@ class SPRINT:
         while isinstance(node, tuple):
             attr_index, attr_type, split_value = node[1]
             if attr_type == 'string':
-                if sample[attr_index] in split_value:
+                if sample[attr_index] == split_value:
                     node = node[2]
                 else:
                     node = node[3]
@@ -27,75 +27,58 @@ class SPRINT:
         return node
 
     def build_tree(self, dataset):
-        is_few_samples = False if self.min_samples is None else len(dataset) <= self.min_samples
-        target_values = set(record[-1] for record in dataset if record[-1])
-        grouped_dataset = [(value, [record for record in dataset if record[-1] == value]) for value in target_values]
-        if len(grouped_dataset) == 1 or is_few_samples:
-            sorted_grouped_dataset = deepcopy(grouped_dataset)
-            sorted_grouped_dataset.sort(key=lambda group: len(group[1]))
-            majority_class = sorted_grouped_dataset[0][0]
-            return majority_class
+        if self.min_samples is not None and len(dataset) <= self.min_samples:
+            return self.majority_class(dataset)
 
+        target_values = set(record[-1] for record in dataset)
+        if len(target_values) == 1:
+            return next(iter(target_values))
+
+        best_split = self.find_best_split(dataset)
+        if best_split is None:
+            return self.majority_class(dataset)
+
+        left_subset, right_subset = self.split_dataset(dataset, best_split)
+        left_node = self.build_tree(left_subset)
+        right_node = self.build_tree(right_subset)
+
+        return (None, best_split, left_node, right_node)
+
+    def majority_class(self, dataset):
+        target_values = [record[-1] for record in dataset]
+        return max(set(target_values), key=target_values.count)
+
+    def find_best_split(self, dataset):
         gini_values = []
         for attr_index, attr_type in enumerate(self.attribute_types):
-            unique_values = list(set(record[attr_index] for record in dataset))
-            unique_values.sort()
-            if attr_type != 'string':
-                split_points = unique_values[:-1]
-            else:
+            unique_values = sorted(set(record[attr_index] for record in dataset))
+            if attr_type == 'string':
                 split_points = unique_values
+            else:
+                split_points = [(unique_values[i] + unique_values[i + 1]) / 2 for i in range(len(unique_values) - 1)]
 
-            if len(split_points) == 0:
-                continue
             for split_value in split_points:
                 split_criteria = (attr_index, attr_type, split_value)
+                left_subset, right_subset = self.split_dataset(dataset, split_criteria)
+                if not left_subset or not right_subset:
+                    continue
 
-                left_subset, right_subset = [], []
-                for record in dataset:
-                    if attr_type == 'string':
-                        if record[attr_index] in split_value:
-                            left_subset.append(record)
-                        else:
-                            right_subset.append(record)
-                    else:
-                        if record[attr_index] <= split_value:
-                            left_subset.append(record)
-                        else:
-                            right_subset.append(record)
+                gini = self.calculate_gini(left_subset, right_subset)
+                gini_values.append((split_criteria, gini))
 
-                left_target_values = set(record[-1] for record in left_subset if record[-1])
-                grouped_left = [(value, [record for record in left_subset if record[-1] == value]) for value in left_target_values]
-                left_gini = 1
-                for (class_label, group) in grouped_left:
-                    proportion = len(group) / len(left_subset)
-                    left_gini -= proportion ** 2
-
-                right_target_values = set(record[-1] for record in right_subset if record[-1])
-                grouped_right = [(value, [record for record in right_subset if record[-1] == value]) for value in right_target_values]
-                right_gini = 1
-                for (class_label, group) in grouped_right:
-                    proportion = len(group) / len(right_subset)
-                    right_gini -= proportion ** 2
-
-                weighted_left_gini = len(left_subset) / len(dataset) * left_gini
-                weighted_right_gini = len(right_subset) / len(dataset) * right_gini
-                total_gini = weighted_left_gini + weighted_right_gini
-
-                gini_values.append((split_criteria, total_gini))
-
-        if len(gini_values) == 0:
-            sorted_grouped_dataset = deepcopy(grouped_dataset)
-            sorted_grouped_dataset.sort(key=lambda group: len(group[1]))
-            majority_class = sorted_grouped_dataset[0][0]
-            return majority_class
+        if not gini_values:
+            return None
 
         best_split = min(gini_values, key=lambda item: item[1])
-        
-        left_subset, right_subset = [], []
-        attr_index, attr_type, split_value = best_split[0]
+        return best_split[0]
+
+    def split_dataset(self, dataset, split_criteria):
+        attr_index, attr_type, split_value = split_criteria
+        left_subset = []
+        right_subset = []
         for record in dataset:
             if attr_type == 'string':
-                if record[attr_index] in split_value:
+                if record[attr_index] == split_value:
                     left_subset.append(record)
                 else:
                     right_subset.append(record)
@@ -104,11 +87,19 @@ class SPRINT:
                     left_subset.append(record)
                 else:
                     right_subset.append(record)
+        return left_subset, right_subset
 
-        left_node = self.build_tree(left_subset)
-        right_node = self.build_tree(right_subset)
-
-        return (None, best_split[0], left_node, right_node)
+    def calculate_gini(self, left_subset, right_subset):
+        total_size = len(left_subset) + len(right_subset)
+        weighted_gini = 0
+        for subset in [left_subset, right_subset]:
+            size = len(subset)
+            if size == 0:
+                continue
+            class_counts = [record[-1] for record in subset]
+            score = sum((class_counts.count(class_val) / size) ** 2 for class_val in set(class_counts))
+            weighted_gini += (1 - score) * (size / total_size)
+        return weighted_gini
 
     def __str__(self):
         return "SPRINT"
